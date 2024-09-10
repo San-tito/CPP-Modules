f6790e6acefd22258a8e46729a843fcefbb476ba
Santi Guzman <sguzman@car6s5.42barcelona.com>
2024-09-10 18:48:37 +0200

    the cpp way

--- CPP-Module-00/ex00/src/megaphone.cpp
+++ CPP-Module-00/ex00/src/megaphone.cpp
@@ -6,22 +6,27 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 15:16:09 by sguzman           #+#    #+#             */
-/*   Updated: 2024/09/09 18:46:55 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/09/10 18:48:08 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+void	loud(void)
+{
+	std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+}
+
+void	touppercase(int argc, char **argv)
+{
+	for (int i = 1; i < argc; i++)
+		for (int j = 0; *(*(argv + i) + j); j++)
+			std::cout << (char)toupper(*(*(argv + i) + j));
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
-		std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		for (int i = 1; i < argc; i++)
-			for (int j = 0; *(*(argv + i) + j); j++)
-				std::cout << (char)toupper(*(*(argv + i) + j));
-	}
+	(argc < 2) ? (loud()) : (touppercase(argc, argv));
 	std::cout << std::endl;
 	return (0);
 }
