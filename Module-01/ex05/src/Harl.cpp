c294166122aed749afbf63172174cd3e9c548539
San-tito <68120901+San-tito@users.noreply.github.com>
2024-10-20 20:42:40 +0200

    Harl 2.0

---
+++ Module-01/ex05/src/Harl.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 15:16:09 by sguzman           #+#    #+#             */
+/*   Updated: 2024/10/20 20:32:39 by santito          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+}
+
+Harl::~Harl(void)
+{
+}
+
+void Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+}
+
+void Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
+}
+
+void Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain(std::string level)
+{
+	const int	size = 4;
+	void (Harl::*func[size])(void)  = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+	const std::string levels[size] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i(0); i < size; i++)
+		(level == levels[i]) ? (this->*func[i])() : static_cast<void>(0); 
+}
---
+++ Module-01/ex05/src/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
+/*   Updated: 2024/10/20 20:39:17 by santito          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl harl;
+
+	harl.complain("");
+	harl.complain("QUITE");
+	harl.complain("DEBUG");
+	harl.complain("ERROR");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	return (0);
+}
