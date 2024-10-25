f1545595318b303868497766283ecfe4175390f3
San-tito <68120901+San-tito@users.noreply.github.com>
2024-10-25 19:51:53 +0200

    Serena, my love

---
+++ Module-03/ex01/include/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
+/*   Updated: 2024/10/25 19:45:28 by santito          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+  public:
+	ClapTrap();
+	ClapTrap(const ClapTrap &copy);
+	ClapTrap(std::string name);
+	ClapTrap &operator=(const ClapTrap &copy);
+	~ClapTrap();
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+  protected:
+	std::string name_;
+	unsigned int hit_points_;
+	unsigned int energy_points_;
+	unsigned int attack_damage_;
+};
+
+#endif /* CLAPTRAP_HPP */
---
+++ Module-03/ex01/src/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
+/*   Updated: 2024/10/25 18:24:45 by santito          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+int	main(void)
+{
+	ScavTrap scav("ScavTrap");
+	scav.attack("target");
+	scav.takeDamage(10);
+	scav.beRepaired(5);
+	return (0);
+}
