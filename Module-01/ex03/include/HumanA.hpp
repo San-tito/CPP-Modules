ddec010b7e64ba228b49a39380e38c303400e3b4
San-tito <68120901+San-tito@users.noreply.github.com>
2024-10-27 21:07:52 +0100

    using naming scheme

--- Module-01/ex03/include/HumanA.hpp
+++ Module-01/ex03/include/HumanA.hpp
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 15:16:09 by sguzman           #+#    #+#             */
-/*   Updated: 2024/10/27 20:44:35 by santito          ###   ########.fr       */
+/*   Updated: 2024/10/27 21:05:58 by santito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ class HumanA
 
   private:
 	std::string m_name;
-	Weapon &m_weapon;
+	Weapon &m_rWeapon;
 };
 
 #endif /* HUMANA_HPP */
--- Module-01/ex03/src/HumanA.cpp
+++ Module-01/ex03/src/HumanA.cpp
@@ -6,14 +6,14 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
-/*   Updated: 2024/10/27 20:53:55 by santito          ###   ########.fr       */
+/*   Updated: 2024/10/27 21:07:17 by santito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
 HumanA::HumanA(std::string name, Weapon &weapon) : m_name(name),
-	m_weapon(weapon)
+	m_rWeapon(weapon)
 {
 }
 
@@ -23,5 +23,5 @@ HumanA::~HumanA(void)
 
 void HumanA::attack(void) const
 {
-	std::cout << m_name << " attacks with their " << m_weapon.getType() << '\n';
+	std::cout << m_name << " attacks with their " << m_rWeapon.getType() << '\n';
 }
