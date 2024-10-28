a47325f0044cdec1c6da5964512effaeb1fe0026
San-tito <68120901+San-tito@users.noreply.github.com>
2024-10-28 11:39:08 +0100

    Serena, my love

--- Module-03/ex01/src/main.cpp
+++ Module-03/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
-/*   Updated: 2024/10/25 18:24:45 by santito          ###   ########.fr       */
+/*   Updated: 2024/10/28 11:31:14 by santito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 
 int	main(void)
 {
-	ScavTrap scav("ScavTrap");
-	scav.attack("target");
-	scav.takeDamage(10);
-	scav.beRepaired(5);
+	ClapTrap techWarrior("TechnoKnight");
+	ScavTrap repairBot("RepairRover");
+	techWarrior.attack("GiantSlime");
+	techWarrior.takeDamage(5);
+	techWarrior.beRepaired(3);
+	repairBot.attack("MightyDragon");
+	repairBot.takeDamage(10);
+	repairBot.beRepaired(5);
+	repairBot.guardGate();
+	ScavTrap cloneBot(repairBot);
+	cloneBot.attack("ShadowBeast");
+	cloneBot.guardGate();
+	techWarrior = repairBot;
+	techWarrior.attack("InfernalWyrm");
 	return (0);
 }
