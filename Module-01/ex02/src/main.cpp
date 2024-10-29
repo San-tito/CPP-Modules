a03accae37c9f207c4525393d6b70da1281c3d1a
Santi Guzman <sguzman@cbr2s3.42barcelona.com>
2024-10-29 16:43:50 +0100

    prefiero hacer born2beroot

--- Module-01/ex02/src/main.cpp
+++ Module-01/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 15:16:09 by sguzman           #+#    #+#             */
-/*   Updated: 2024/10/27 20:39:43 by santito          ###   ########.fr       */
+/*   Updated: 2024/10/29 16:43:17 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int	main(void)
 	std::string string("HI THIS IS BRAIN");
 	std::string *stringPTR(&string);
 	std::string &stringREF(string);
-	std::cout << "Memory address of string variable:" << &string << std::endl;
-	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
-	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
-	std::cout << "Value of string variable: " << string << std::endl;
-	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
-	std::cout << "Value referred to by stringREF: " << stringREF << std::endl;
+	std::cout << "Memory address of string variable: " << &string << '\n';
+	std::cout << "Memory address held by stringPTR: " << stringPTR << '\n';
+	std::cout << "Memory address held by stringREF: " << &stringREF << '\n';
+	std::cout << "Value of string variable: " << string << '\n';
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << '\n';
+	std::cout << "Value referred to by stringREF: " << stringREF << '\n';
 	return (0);
 }
