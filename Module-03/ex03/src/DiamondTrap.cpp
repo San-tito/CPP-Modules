64bbe47dd6d287560816fea1ea7f1a5b43405ae3
San-tito <68120901+San-tito@users.noreply.github.com>
2024-10-30 10:10:58 +0100

    Diamond has canonical form

--- Module-03/ex03/src/DiamondTrap.cpp
+++ Module-03/ex03/src/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
-/*   Updated: 2024/10/28 12:23:57 by santito          ###   ########.fr       */
+/*   Updated: 2024/10/30 10:10:42 by santito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,23 @@ DiamondTrap::DiamondTrap(void) : ClapTrap("DefaultDiamond_clap_name"),
 DiamondTrap::DiamondTrap(const std::string &name) : ClapTrap(name
 	+ "_clap_name"), FragTrap(name), ScavTrap(name), m_name(name)
 {
-	m_hitPoints = FragTrap::m_hitPoints; 
-    m_energyPoints = ScavTrap::m_energyPoints; 
-    m_attackDamage = FragTrap::m_attackDamage;
+	m_hitPoints = FragTrap::m_hitPoints;
+	m_energyPoints = ScavTrap::m_energyPoints;
+	m_attackDamage = FragTrap::m_attackDamage;
 	std::cout << "DiamondTrap constructor called for " << m_name << "\n";
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap &other) : ClapTrap(other),
+	FragTrap(other), ScavTrap(other), m_name(other.m_name)
+{
+	std::cout << "DiamondTrap copy constructor called for " << m_name << "\n";
+}
+
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &other)
+{
+	ClapTrap::operator=(other);
+	m_name = other.m_name;
+	return (*this);
 }
 
 DiamondTrap::~DiamondTrap(void)
