c9d837bc59a8152f64ba1b0bb2137e14fd056244
San-tito <68120901+San-tito@users.noreply.github.com>
2024-10-30 10:07:55 +0100

    Revert "Now it’s"
    This reverts commit 47a91a7c14b83b851d70635f1b1238834bb0f1ac.

--- Module-03/ex03/include/DiamondTrap.hpp
+++ Module-03/ex03/include/DiamondTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
-/*   Updated: 2024/10/28 12:32:05 by santito          ###   ########.fr       */
+/*   Updated: 2024/10/28 12:17:07 by santito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ class DiamondTrap : public FragTrap, public ScavTrap
   public:
 	DiamondTrap(void);
 	DiamondTrap(const std::string &name);
-	DiamondTrap(const DiamondTrap &other);
-	DiamondTrap &operator=(const DiamondTrap &other);
 	~DiamondTrap(void);
 
 	void attack(const std::string &target);
