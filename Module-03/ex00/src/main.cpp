0e838acf7875e66bb384e3e835b3a0c08367d644
Santi Guzman <sguzman@cbr3s5.42barcelona.com>
2024-11-07 12:11:05 +0100

    Aaaaand... OPEN

--- Module-03/ex00/src/main.cpp
+++ Module-03/ex00/src/main.cpp
@@ -6,22 +6,28 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
-/*   Updated: 2024/11/04 23:05:27 by santito          ###   ########.fr       */
+/*   Updated: 2024/11/07 12:09:21 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "ClapTrap.hpp"
 
 int	main(void)
 {
-	ClapTrap hero("Hero");
-	ClapTrap villain("Villain");
-	hero.attack("Villain");
-	villain.takeDamage(0);
-	villain.attack("Hero");
-	hero.takeDamage(0);
-	hero.beRepaired(5);
-	hero.attack("Villain");
-	villain.takeDamage(0);
+	ClapTrap bender("Bender");
+	bender.attack("Zap Brannigan");
+	bender.takeDamage(4);
+	bender.beRepaired(2);
+	std::cout << '\n';
+	ClapTrap zoidberg("Dr. Zoidberg");
+	zoidberg.attack("Leela");
+	zoidberg.takeDamage(6);
+	zoidberg.beRepaired(3);
+	std::cout << '\n';
+	ClapTrap fry(zoidberg);
+	fry.attack("Bender");
+	fry.takeDamage(5);
+	std::cout << '\n';
+	ClapTrap leela(bender);
+	leela.attack("Zoidberg");
 	return (0);
 }
