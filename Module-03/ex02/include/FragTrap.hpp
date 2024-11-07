ee63ac026a7bc6222dbd001bd615fd7b4a0ce2f5
Santi Guzman <sguzman@cbr3s5.42barcelona.com>
2024-11-07 12:29:30 +0100

    Repetitive work

--- Module-03/ex02/include/FragTrap.hpp
+++ Module-03/ex02/include/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
-/*   Updated: 2024/10/28 11:50:42 by santito          ###   ########.fr       */
+/*   Updated: 2024/11/07 12:25:23 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,10 @@ class FragTrap : public ClapTrap
 {
   public:
 	FragTrap(void);
-	FragTrap(const std::string& name);
+	FragTrap(const std::string &name);
 	~FragTrap(void);
 
-	void attack(const std::string &target);
-	void highFivesGuys();
+	void highFivesGuys(void);
 };
 
 #endif /* FRAGTRAP_HPP */
