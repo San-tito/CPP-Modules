d162ac272238861fc5d2c6facbc9db72161ede2a
Santi Guzman <sguzman@cbr3s5.42barcelona.com>
2024-11-07 12:37:00 +0100

    Repetitive work

--- Module-03/ex02/src/main.cpp
+++ Module-03/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:58:41 by sguzman           #+#    #+#             */
-/*   Updated: 2024/10/28 11:50:59 by santito          ###   ########.fr       */
+/*   Updated: 2024/11/07 12:28:16 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,14 @@
 
 int	main(void)
 {
-	ClapTrap techWarrior("TechnoKnight");
-	ScavTrap repairBot("RepairRover");
-	FragTrap fragHero("FragMaster");
-	techWarrior.attack("GiantSlime");
-	techWarrior.takeDamage(5);
-	techWarrior.beRepaired(3);
-	repairBot.attack("MightyDragon");
-	repairBot.takeDamage(10);
-	repairBot.beRepaired(5);
-	repairBot.guardGate();
-	fragHero.attack("ShadowBeast");
-	fragHero.takeDamage(15);
-	fragHero.beRepaired(10);
-	fragHero.highFivesGuys();
-	FragTrap cloneHero(fragHero);
-	cloneHero.attack("InfernalWyrm");
-	cloneHero.highFivesGuys();
-	techWarrior = fragHero;
-	techWarrior.attack("DragonKing");
+	FragTrap frag("Boomer");
+	frag.attack("Scavenger");
+	frag.takeDamage(50);
+	frag.beRepaired(20);
+	frag.highFivesGuys();
+	std::cout << '\n';
+	FragTrap frag_copy(frag);
+	frag_copy.attack("Bender");
+	frag_copy.takeDamage(30);
 	return (0);
 }
